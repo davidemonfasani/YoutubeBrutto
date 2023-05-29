a7ed1eecb66c9c70c1f36ce715988184e9db9024
davidemonfasani <davide.monfasani.studenti@isii.it>
2023-05-29 12:56:56 +0200

    saaa

--- src/app/pages/upload-video/upload-video.component.ts
+++ src/app/pages/upload-video/upload-video.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserAuthService } from 'src/app/services/user-auth.service';
 
 @Component({
   selector: 'app-upload-video',
@@ -6,5 +10,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./upload-video.component.css']
 })
 export class UploadVideoComponent {
+  UploadForm: FormGroup;
 
+  constructor(
+    private router: Router,
+    private http: HttpClient,
+    public auth: UserAuthService,
+    private formBuilder: FormBuilder
+  ) {
+    this.UploadForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      nome: ['', Validators.required],
+      cognome: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+  username = ''
+  cognome = ''
+  email = ''
+  password = ''
+  nome = ''
+  image : any
+  Upload(){
+    
+  }
 }
