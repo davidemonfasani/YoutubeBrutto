d7d568fe3cb627f26bc2dda8cc802962defaa6c4
davidemonfasani <davide.monfasani.studenti@isii.it>
2023-05-30 16:02:42 +0200

    Se parli ancora pere di faccio diventare una pera

--- src/app/components/appbar/appbar.component.ts
+++ src/app/components/appbar/appbar.component.ts
@@ -4,6 +4,7 @@ import { UserAuthService } from '../../services/user-auth.service';
 import { RoutesService } from 'src/app/services/routes.service';
 import { VideoService } from 'src/app/services/video.service';
 import { Video } from 'src/app/interfaces/video';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 
 @Component({
@@ -12,14 +13,18 @@ import { Video } from 'src/app/interfaces/video';
   styleUrls: ['./appbar.component.css']
 })
 export class AppbarComponent {
+  SearchForm: FormGroup;
   constructor(private router: Router,
      public routesService: RoutesService,
      private videoService : VideoService,
      private UtenteAuth:UserAuthService,
-     ) {
+     private formBuilder: FormBuilder,
+  ) {
+    this.SearchForm = this.formBuilder.group({
+      Search: ['', Validators.required],
+    });
   }
   firstClick = true
-  ricercaValue = ''
 
 
 Userlogged(){
@@ -38,7 +43,7 @@ Userlogged(){
   }
 
   search() {
-    this.videoService.filterVideo(this.ricercaValue)
+    this.videoService.filterVideo(this.SearchForm.value.Search)
     .subscribe((result: Video[]) => {
       console.log('ciaoo')
     });
